afca56f9afa4a402fbb2915879e7397011197a92
Iqbal <a.iqbalcs129@gmail.com>
2025-12-01 12:38:31 +0800

    Create a shared folder and new vite project

---
+++ app/shared/constants/Colors.ts
@@ -0,0 +1,11 @@
+/*
+Colors used for styling in the conditions where normal tailwind is unusable.
+The colors here should match those in tailwind.config.js.
+*/
+
+export const Colors = {
+  bg1: "#00495cff",
+  accent1: "#701a1aff",
+  primary: "#535252",
+  secondary: "#f4f4f4"
+};
---
+++ app/shared/constants/Status.ts
@@ -0,0 +1,16 @@
+/*
+These are enum for permit statuses used throughout the app used to avoid inconsistency.
+*/
+
+export enum PermitStatus {
+  DRAFT = "DRAFT",
+  SUBMITTED = "SUBMITTED",
+  APPROVED = "APPROVED",
+  PENDING = "PENDING",
+  WAITING = "WAITING",
+  REJECTED = "REJECTED",
+  ACTIVE = "ACTIVE",
+  COMPLETED = "COMPLETED",
+}
+
+export const PERMIT_STATUS_LIST = Object.values(PermitStatus);
